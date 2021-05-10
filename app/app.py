f991b92225fd0e22486835adc4ee196901f8045a
Justin Nietzer <justinnietzer@gmail.com>
2021-05-10 19:59:29 -0400

    Fix: Nav bar links work in sendgrid.html

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
-app.config['MAIL_DEFAULT_SENDER'] = 'justinnietzer@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'nietzersche@gmail.com'
 mail = Mail(app)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
