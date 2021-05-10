de19501797fd18853da6846ecf1498d87cc72133
Justin Nietzer <justinnietzer@gmail.com>
2021-05-10 11:49:41 -0400

    Fix: vulnerability

--- app/app.py
+++ app/app.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = "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"
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = 'justinnietzer@gmail.com'
 mail = Mail(app)
 
